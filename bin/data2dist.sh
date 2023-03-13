8257b203e3f0a16e9d046ebf60078af03819e494
Zion Ng <zion@intzone.com>
2023-03-13 10:58:50 +0800

    Rename shell script for generating dist files

--- bin/data2dist.sh
+++ bin/data2dist.sh
@@ -2,7 +2,7 @@
 # sh used instead of bash for portability & maximum compatibility.
 
 ##
-# Convert JSON files to JavaScript files for easy embedding in webpage without worrying about CORS.
+# Convert JSON data files to JavaScript distributable files for easy embedding in webpage without worrying about CORS.
 #
 # Instead of setting the JSON contents to a variable, the JavaScript file runs
 # a function that will emit a `songbook.ready` event of CustomEvent type on
@@ -10,7 +10,7 @@
 # be used to retrieve the JSON contents, instead of guessing whether the file
 # has loaded or not.
 #
-# @example Run this script from the root of this repository: bin/json2js.sh
+# @example Run this script from the root of this repository: bin/data2dist.sh
 ##
 
 # Check if in correct directory
@@ -26,7 +26,10 @@ for FILE_PATH in data/*.json; do
         continue;
     fi
 
-    CONTENTS="(function (currentScript) { "
+    # Add just 1 line before and after the JSON, to make it easy to manually copy out the JSON
+    # Do NOT attempt to sanitize JSON, i.e. replace smart punctuation “”″‘’′—– with plain ASCII, as
+    # it is not binary-safe & will mess up the multi-byte Unicode characters for the Chinese lyrics
+    CONTENTS="(function (currentScript) { " # no \n here
     CONTENTS="${CONTENTS}window.dispatchEvent(new CustomEvent('songbook.ready', { detail: { data: // Added line\n"
     CONTENTS="${CONTENTS}$(cat ${FILE_PATH})"
     CONTENTS="${CONTENTS}\n}})); })(document.currentScript); // Added line" # don't add \n cos echo will add \n
