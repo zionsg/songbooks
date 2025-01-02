55daa42ff96adf188ab2cbb7b11859b1920dab8a
Zion Ng <zion@intzone.com>
2025-01-02 10:19:58 +0800

    Do not ignore dist/

---
+++ dist/miscellaneous.js
@@ -0,0 +1,115 @@
+(function (currentScript) { window.dispatchEvent(new CustomEvent('songbook.ready', { detail: { data: // Added line
+{
+  "json": {
+    "source": "https://github.com/zionsg/songbooks/blob/master/data/miscellaneous.json",
+    "schema": "https://github.com/zionsg/songbooks/blob/master/data/songbook.schema.json"
+  },
+  "bookPrefix": "MISC",
+  "title": {
+    "en": "Miscellaneous Hymns",
+    "cn": "各种诗歌"
+  },
+  "copyright": "",
+  "sections": {
+    "All": {
+      "title": {
+        "cn": "全部"
+      },
+      "songList": [
+        [
+          "*"
+        ]
+      ]
+    },
+  },
+  "songs": {
+    "We Wise Men of Orient Are": {
+      "title": {
+        "cn": "东方三博士"
+      },
+      "music": {
+        "composers": [
+          "John H. Hopkins"
+        ],
+        "tune": "Kings of Orient",
+        "keySignature": [
+          "G"
+        ],
+        "timeSignature": [
+          "3/8"
+        ]
+      },
+      "lyrics": {
+        "authors": [
+          "John H. Hopkins"
+        ],
+        "en": {
+          "1": [
+            "We wise men of Orient are:",
+            "Bearing gifts we traverse afar",
+            "Field and fountain, moor and mountain,",
+            "Following yonder star."
+          ],
+          "2": [
+            "Born a King on Bethlehem's plain,",
+            "Gold I bring to crown Him again,",
+            "King for ever, ceasing never",
+            "Over us all to reign.",
+          ],
+          "3": [
+            "Frankincense to offer have I;",
+            "Incense owns a Deity nigh;",
+            "Prayer and praising all men raising,",
+            "Worship Him, God on high.",
+          ],
+          "4": [
+            "Myrrh is mine: its bitter perfume",
+            "Breathes a life of gathering gloom:",
+            "Sorrowing, sighing, bleeding, dying,",
+            "Sealed in the stone-cold tomb.",
+          ],
+          "chorus": [
+            "O star of wonder, star of night,",
+            "Star with royal beauty bright,",
+            "Westward leading, still proceeding,",
+            "Guide us to Thy perfect light."
+          ]
+        },
+        "cn": {
+          "1": [
+            "三博士行程自东方；",
+            "携带礼物远道前往，",
+            "经过田野江河山岗，",
+            "跟随景星明光。"
+          ],
+          "2": [
+            "新王降生伯利恒城，",
+            "奉献金冕贺他为王，",
+            "掌握权柄治理万邦，",
+            "世人蒙他恩光。",
+          ],
+          "3": [
+            "我今献上乳香奇珍；",
+            "认识真神常与亲近；",
+            "愿万人来祷告颂赞，",
+            "朝拜真神在天。",
+          ],
+          "4": [
+            "没药味难苦却甘芳；",
+            "深思主为我罪舍身：",
+            "忧愁，叹息，流血死亡，",
+            "埋葬封在坟墓。",
+          ],
+          "chorus": [
+            "啊！奇妙星，夜放辉煌，",
+            "发出灿烂大光芒，",
+            "带领我们步向西方，",
+            "直见主完全面旁。"
+          ]
+        }
+      },
+      "notes": []
+    }
+  }
+}
+}})); })(document.currentScript); // Added line
